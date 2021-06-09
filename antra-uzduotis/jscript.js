6e2b712162f9441c3e08532535c969ad687b0000
ginotas <ginotas@gmail.com>
2021-06-09 18:06:41 +0300

    namu darbai number 2

---
+++ antra-uzduotis/jscript.js
@@ -0,0 +1,76 @@
+"use strict"
+
+console.log("hello");
+
+// 1. Suprogramuokite skriptą, kurioje parašius tam tikrą sekundžių kiekį, 
+// rodo  24 valandų, minučių, sekundžių formatu.
+
+/*  nesigauna ideti i
+function myFunction() {
+    document.getElementById("sekundesx").value;
+    document.getElementById("rezultatas").innerHTML = ("valandos: " + valandos + " minutes: " + minutes + " sekundes: " +sekundes);;
+  }
+  let sekundesx = document.getElementById("sekundesx").value;
+*/
+
+let sekundesx = 1000;
+let valandos = Math.floor(sekundesx / 3600);
+sekundesx %= 3600;
+let minutes = Math.floor(sekundesx / 60);
+let sekundes = sekundesx % 60;
+
+console.log("valandos: " + valandos + " minutes: " + minutes + " sekundes: " +sekundes);
+
+
+
+// 2. Suraskite triženklio skaičiaus visų skaitmenų:
+// suma, vidurki ir sandauga. 
+// Prieš tai atpažinti, 
+// ar tai yra triženklis skaičius, jei skaičius dviženklis ar n-ženklis, veiksmų nevykdyti 
+
+
+
+//skaiciuoja suma, o kodel - nzn :D
+
+var value = 999,
+    sum = 0;
+    
+while (value) {
+    sum += value % 10;
+    value = Math.floor(value / 10);
+}
+
+console.log("triju skaiciu suma: " + sum);
+
+// vidurki rasti kai skaiciuojama suma lengviau;
+
+
+var avg = 0;
+
+avg = sum / 3;
+
+console.log("triju skaiciu sumos vidurkis yra: " + avg);
+
+// sandauga... rytoj bandysiu
+
+
+
+
+//kvadratine lygtis
+
+/*
+var a, b, c;
+
+a = 2;
+b =
+
+
+
+
+
+
+*/
+
+
+
+
